228dfd4ca949654c5d1c1a97f28c63dbd75dbecb
Dev <kt.rajasekar@gmail.com>
2017-06-02 12:41:14 +0530

    update

---
+++ app/scripts/common.js
@@ -0,0 +1,6 @@
+angular.module('exeApp').directive("myWidget", function() {
+return {
+restrict: "E",
+template: "<p>Hello World</p>"
+};
+});
--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -8,15 +8,12 @@
  * Controller of the exeApp
  */
 angular.module('exeApp')
-  .controller('AboutCtrl', function () {
+  .controller('AboutCtrl', function ($scope) {
 
+    $scope.$watch(function() {
+    return $scope.name;
+    }, function(newValue, oldValue) {
+    console.log("change detected: " + newValue)
+    });
 
   });
-
-
-  angular.module('exeApp').directive("myWidget", function() {
-return {
-restrict: "E",
-template: "<p>Hello World</p>"
-};
-});
